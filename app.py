6e0f28b12d6302fe1a128e768029ce2c883c96c5
Divisi_Hotel_MTT <d4t4m1tr4@gmail.com>
2026-05-17 19:56:21 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1061,7 +1061,7 @@ if st.session_state["tab"] == "input":
         st.markdown('<div class="bb-wrap">',unsafe_allow_html=True)
         _ne_run   = st.button("Submit",type="primary",use_container_width=True,
             disabled=not _ne_ready,key="ne_run")
-        _ne_clear = st.button("Hapus Hasil",type="secondary",use_container_width=True,key="ne_clear")
+        _ne_clear = st.button("Hapus",type="secondary",use_container_width=True,key="ne_clear")
         st.markdown('</div>',unsafe_allow_html=True)
 
         if not _ne_ready:
