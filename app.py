bd3d2e6b8e341ccde0cf92e8752506fccbf4975f
Divisi_Hotel_MTT <d4t4m1tr4@gmail.com>
2026-05-17 19:55:04 +0700

    Update app.py

--- app.py
+++ app.py
@@ -898,7 +898,7 @@ if st.session_state["tab"] == "input":
         st.markdown('<div class="bb-wrap">',unsafe_allow_html=True)
         _run = st.button("Submit",type="primary",use_container_width=True,
             disabled=(not _n or not bulk_issuer or not bulk_pic.strip()),key="bulk_run")
-        _clear = st.button("Hapus Hasil",type="secondary",use_container_width=True,key="bulk_clear")
+        _clear = st.button("Delete",type="secondary",use_container_width=True,key="bulk_clear")
         st.markdown('</div>',unsafe_allow_html=True)
 
         if _clear:
